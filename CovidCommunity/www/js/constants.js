c6abc2e62d13eb8f16fa4bd6226efa3124648db3
yliankuo <yliankuo@yahoo.com>
2020-09-19 06:03:52 -0400

    Initial Version
    Framework for the app, setup pages for the webapp

---
+++ CovidCommunity/www/js/constants.js
@@ -0,0 +1,13 @@
+/**
+ * Namespace for all constants in the application.
+ * @namespace Constants
+ */	
+var Constants = {
+	statPage : "html/stats.html",
+	loginPage : "index.html",
+	privacyPage : "html/privacy.html",
+	mapPage : "html/maps.html",
+	newPage : "html/news.html",
+	notifPage : "html/notif.html",
+	dailyPage : "html/daily.html"
+}
---
+++ CovidCommunity/www/js/helpers.js
@@ -0,0 +1,6 @@
+/**
+ * Namespace for all helper functions in application
+ * @namespace Helpers
+ */	
+var Helpers = {
+}
