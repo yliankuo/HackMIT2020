b546b487bcc8baf0c3e00fc568fbfe6919a07dfc
Brandon Xiao <bxiao2000@gmail.com>
2020-09-19 20:43:36 -0400

    making login page

--- CovidCommunity/www/js/index.js
+++ CovidCommunity/www/js/index.js
@@ -5,13 +5,23 @@
 // -----Local variable declaration start-----
 // If you want to pass variables between scripts use LocalStorage
 
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
 
 // -----Local variable declaration end-----
 
 
 // -----Listeners Start-----
-document.addEventListener('deviceready', onDeviceReady, false);
+/*document.addEventListener('deviceready', onDeviceReady, false);
 
 // -----Listeners End-----
 
@@ -28,6 +38,6 @@ function submitCredentials(){
 
 
 	location.href = Constants.statPage;
-}
+}*/
 // -----Functions End-----
 
